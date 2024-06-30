68094e464ac3fc8112d3ed3786333270b16f8d43
Ali Tamer <alitamer82.at@gmail.com>
2024-07-01 00:49:05 +0300

    fix yes

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import pandas as pd
 import joblib
-import gradio as gr
 from flask import Flask, request, jsonify
 
-# Load the saved ARIMA model
 model = joblib.load('arima_model.pkl')
-
-# Function to generate predictions for new data
 
 
 def make_predictions(input_data):
     try:
-        # Convert input data to DataFrame
         new_data = pd.DataFrame(input_data)
-
-        # Check if the new data has only one column
         if new_data.shape[1] != 1:
             raise ValueError("Input data must have exactly one column")
 
-        # Make predictions using the trained model
         predictions = model.predict(n_periods=len(new_data))
-        return predictions.tolist()  # Convert to list for API response
+        return predictions.tolist()
 
     except Exception as e:
         return {"error": str(e)}
@@ -49,10 +41,5 @@ def predict():
         return jsonify({"error": str(e)}), 400  # Return 400 for bad requests
 
 
-# Create Gradio interface for the prediction function
-interface = gr.Interface(fn=make_predictions, inputs=gr.Dataframe(
-    headers=["column1"], datatype="number"), outputs="json")
-interface.launch(share=True)
-
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
